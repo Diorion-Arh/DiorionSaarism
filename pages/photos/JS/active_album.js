bf9ddd7ea9c8e8d6e2ef3b376e6b0be1905a1517
Diorion-Arh <raimon123ezake@gmail.com>
2020-06-24 17:43:04 +0300

    глобальные изменения - добавление новой страницы фотографии

---
+++ pages/photos/JS/active_album.js
@@ -0,0 +1,50 @@
+let content = document.getElementById("open_album");
+let open = document.getElementById("active_album");
+let close = document.getElementById("close_album");
+open.addEventListener("click", () => {
+    content.style.display = "block"
+
+});
+close.addEventListener("click", () => {
+    content.style.display = "none"
+});
+let content2 = document.getElementById("open_album2");
+let open2 = document.getElementById("active_album2");
+let close2 = document.getElementById("close_album2");
+open2.addEventListener("click", () => {
+    content2.style.display = "block"
+
+});
+close2.addEventListener("click", () => {
+    content2.style.display = "none"
+});
+let content3 = document.getElementById("open_album3");
+let open3 = document.getElementById("active_album3");
+let close3 = document.getElementById("close_album3");
+open3.addEventListener("click", () => {
+    content3.style.display = "block"
+
+});
+close3.addEventListener("click", () => {
+    content3.style.display = "none"
+});
+let content4 = document.getElementById("open_album4");
+let open4 = document.getElementById("active_album4");
+let close4 = document.getElementById("close_album4");
+open4.addEventListener("click", () => {
+    content4.style.display = "block"
+
+});
+close4.addEventListener("click", () => {
+    content4.style.display = "none"
+});
+let content5 = document.getElementById("open_album5");
+let open5 = document.getElementById("active_album5");
+let close5 = document.getElementById("close_album5");
+open5.addEventListener("click", () => {
+    content5.style.display = "block"
+
+});
+close5.addEventListener("click", () => {
+    content5.style.display = "none"
+});
